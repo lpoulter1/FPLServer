3a35db9a66790c76f22b4e4b10f5d41b5f6534cd
James Attrill <attrill20@gmail.com>
2023-08-06 18:02:32 +0100

    new attempt

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,24 +1,7 @@
-// api/index.js
-const fetch = import('node-fetch').then((mod) => mod.default);
+// pages/api/index.js
+import fetch from 'isomorphic-unfetch';
 
-// ... Rest of the code remains the same ...
-
-
-// Serverless function handler
-module.exports = async (req, res) => {
-  // Add CORS headers to allow cross-origin requests
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-  );
-  // Check if the request method is OPTIONS (preflight request)
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
+export default async (req, res) => {
   try {
     const bootstrapResponse = await fetch(
       'https://fantasy.premierleague.com/api/bootstrap-static/'
@@ -35,15 +18,9 @@ module.exports = async (req, res) => {
       fixturesData: fixturesData,
     };
 
-     // Log responseData to the console to check if it's working
-     console.log('Response Data:', responseData);
-
-    // Send the response
     res.status(200).json(responseData);
   } catch (error) {
-    // Handle errors
     console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Something went wrong' });
   }
 };
-
