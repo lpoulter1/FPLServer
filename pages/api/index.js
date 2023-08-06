7c59e1a033f5c19648ca706c962de63dbcf3a073
Laurie Poulter <32073440+lpoulter1@users.noreply.github.com>
2023-08-06 18:40:25 +0100

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,19 +1,17 @@
 // pages/api/index.js
-async function proxy (req, res) => {
+async function proxy(req, res) {
   try {
     const bootstrapResponse = await fetch(
-      'https://fantasy.premierleague.com/api/bootstrap-static/'
+      'https://fantasy.premierleague.com/api/bootstrap-static/',
     );
     const bootstrapData = await bootstrapResponse.json();
 
-    const fixturesResponse = await fetch(
-      'https://fantasy.premierleague.com/api/fixtures/'
-    );
+    const fixturesResponse = await fetch('https://fantasy.premierleague.com/api/fixtures/');
     const fixturesData = await fixturesResponse.json();
 
     const responseData = {
-      bootstrapData: bootstrapData,
-      fixturesData: fixturesData,
+      bootstrapData,
+      fixturesData,
     };
 
     res.status(200).json(responseData);
@@ -21,24 +19,23 @@ async function proxy (req, res) => {
     console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Something went wrong' });
   }
-};
-
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
 }
 
-export default allowCors(proxy)
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return fn(req, res);
+};
 
+export default allowCors(proxy);
